7f7e2fb3f88e4eb8e76e26051f43db5b879458c4
Thomas Axelsson <thomasa88@gmail.com>
2022-01-24 14:14:56 +0100

    Use CSV for account mapping

---
+++ csvmapping.py
@@ -0,0 +1,33 @@
+import csv
+import os
+from collections import defaultdict
+import logging
+
+FILE_DIR = os.path.dirname(os.path.realpath(__file__))
+
+account_mapping = defaultdict(list)
+
+with open(FILE_DIR + '/mapping.csv', newline='') as csv_file:
+    reader = csv.reader(csv_file)
+
+    headers = next(reader)
+    column_mapping = defaultdict(list)
+    for i, header in enumerate(headers):
+        system, _, subinfo = header.partition('-')
+        column_mapping[system].append(i)
+
+    for row in reader:
+        for system, columns in column_mapping.items():
+            account = tuple(row[c] for c in columns)
+            empty = True
+            for c in account:
+                if c:
+                    empty = False
+                    break
+            if empty:
+                # No data on this line. It should be ignored
+                # when mapped to this system.
+                account = None
+            account_mapping[system].append(account)
+
+logging.debug(f'CSV Account mapping {account_mapping}')
